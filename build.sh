388fb9dd286117d1415ffa6c4604e9997e29a741
Danct12 <danct12@disroot.org>
2020-04-17 00:01:37 +0700

    XiaomiMI6Pkg: Rename to RedmiNote7Pkg and initial bringup
    Bringing up the Redmi Note 7 device, a device released in 2019.
    
    The Redmi Note 7 has:
    - SoC: Qualcomm Snapdragon 660 (SDM660/MSM8976 Plus)
    - GPU: Adreno 512
    - RAM: 2/3/4GB (LPDDR4x)
    - ROM: 16/32/64GB (eMMC 5.1)
    
    As discussed on DanctNIX Discord, this device is kinda different
    from other devices that currently has a EDK2 port (e.g. SDM835/845),
    and as well as it is using eMMC, not UFS..
    
    Signed-off-by: Danct12 <danct12@disroot.org>

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p XiaomiMI6Pkg/XiaomiMI6Pkg.dsc
-gzip -c < workspace/Build/XiaomiMI6Pkg/DEBUG_GCC5/FV/XIAOMIMI6PKG_UEFI.fd >uefi_image
-cat sagit.dtb >>uefi_image
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p RedmiNote7Pkg/RedmiNote7Pkg.dsc
+gzip -c < workspace/Build/RedmiNote7Pkg/DEBUG_GCC5/FV/REDMINOTE7PKG_UEFI.fd >uefi_image
+cat lavender.dtb >>uefi_image
 abootimg --create uefi.img -k uefi_image -r ramdisk-null -f bootimg.cfg
